d68330311f928c11abe88ff0987931feb1b6befc
Charlotto <cooder_z@outlook.com>
2025-05-10 09:53:51 +0800

    small changes

--- src/main/java/com/zplus/terablog/TeraBlogApplication.java
+++ src/main/java/com/zplus/terablog/TeraBlogApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- *   
 
- */
 @SpringBootApplication
 @EnableScheduling
 public class TeraBlogApplication
